cf9a46f212cb18c72160c444b9dcb15b66ee7b43
Juan Neira <jincastillo992@gmail.com>
2023-10-19 04:17:51 -0300

    Feature 2
    Agregado uuid package, funciones varias para ToDo

--- src/todo/app.js
+++ src/todo/app.js
@@ -1,13 +1,49 @@
 import html from './app.html?raw';
+import todoStore from '../store/todo.store';
+import { renderTodos } from './use-cases';
+
+const elementIDs = {
+    TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
+};
 /**
  *
  * @param {String} elementId
  */
 export const App = (elementId) => {
+    const displayTodos = () => {
+        const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+        renderTodos(elementIDs.TodoList, todos);
+    };
     //Cuando la funcion App se llama
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
+        displayTodos();
     })();
+    // Referencias al HTML
+    const newDescriptionInput = document.querySelector(elementIDs.NewTodoInput);
+    const todoListUl = document.querySelector(elementIDs.TodoList);
+
+    // Listeners
+    newDescriptionInput.addEventListener('keyup', (event) => {
+        if (event.keyCode !== 13) return;
+        if (event.target.value.trim().length === 0) return;
+        todoStore.addTodo(event.target.value);
+        displayTodos();
+        event.target.value = '';
+    });
+    todoListUl.addEventListener('click', (event) => {
+        const element = event.target.closest('[data-id]');
+        todoStore.toggleTodo(element.getAttribute('data-id'));
+        displayTodos();
+    });
+    todoListUl.addEventListener('click', (event) => {
+        const isDestroyElement = event.target.className === 'destroy';
+        const element = event.target.closest('[data-id]');
+        if (!element || !isDestroyElement) return;
+        todoStore.deleteTodo(element.getAttribute('data-id'));
+        displayTodos();
+    });
 };
